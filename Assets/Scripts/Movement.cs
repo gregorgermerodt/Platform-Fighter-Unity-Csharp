bab12ea63a1e8ce310fc1d5005c2ce5eb4e9f28c
Nudelhuhn <150367128+Nudelhuhn@users.noreply.github.com>
2024-07-02 17:15:07 +0200

    PhysicsEngine Skript Angepasst

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,49 +13,66 @@ public class Movement : MonoBehaviour
     public float jumpHeight = 3.0f;
     public static bool isGrounded;
 
+    private PhysicsEngine physicsEngine;
+
     private void Start()
     {
-        charController = GetComponent<CharacterController>();
+        //charController = GetComponent<CharacterController>();
+        physicsEngine = GetComponent<PhysicsEngine>();
     }
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && gravityVelocity.y < 0)
-        {
-            gravityVelocity.y = -2.0f;
-        }
+        //if (isGrounded && gravityVelocity.y < 0)
+        //{
+        //    gravityVelocity.y = -2.0f;
+        //}
 
         Vector2 inputVector = new Vector2(0.0f, 0.0f);
 
-        if (Input.GetKey(KeyCode.W))
-            inputVector += new Vector2(0.0f, 1.0f);
+        //if (Input.GetKey(KeyCode.W))
+            //inputVector += new Vector2(0.0f, 1.0f);
+            //physicsEngine.SetVerticalVelocity(1.0f);
+            //physicsEngine.SetVelocityVector(0.0f, 1.0f);
         if (Input.GetKey(KeyCode.A))
-            inputVector += new Vector2(-1.0f, 0.0f);
-        if (Input.GetKey(KeyCode.S))
-            inputVector += new Vector2(0.0f, -1.0f);
+            //inputVector += new Vector2(-1.0f, 0.0f);
+            physicsEngine.SetVelocity(new Vector2(-1.0f, 0.0f));
+        //physicsEngine.SetVelocityVector(-1.0f, 0.0f);
+        //if (Input.GetKey(KeyCode.S))
+        //inputVector += new Vector2(0.0f, -1.0f);
+        //physicsEngine.SetVerticalVelocity(-1.0f);
+        //physicsEngine.SetVelocityVector(0.0f, -1.0f);
         if (Input.GetKey(KeyCode.D))
-            inputVector += new Vector2(1.0f, 0.0f);
-        if (Input.GetButtonDown("Jump") && isGrounded)
-            gravityVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityAcceleration);
-
-        float elapsedTime = 0f;
-        float timeToElapse = 5f;
-        while (elapsedTime < timeToElapse)
+            //inputVector += new Vector2(1.0f, 0.0f);
+            physicsEngine.SetVelocity(new Vector2(1.0f, 0.0f));
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            elapsedTime += Time.deltaTime;
+            physicsEngine.SetVelocity(new Vector2(0.0f, 0.0f));
         }
+        //physicsEngine.SetVelocityVector(1.0f, 0.0f);
+        //if (Input.GetButtonDown("Jump") && isGrounded)
+        //gravityVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityAcceleration);
+        if (Input.GetButtonDown("Jump"))
+            physicsEngine.Jump();
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0.0f, inputVector.y).normalized;
+        //float elapsedTime = 0f;
+        //float timeToElapse = 5f;
+        //while (elapsedTime < timeToElapse)
+        //{
+        //    elapsedTime += Time.deltaTime;
+        //}
 
-        if(moveDir.magnitude >= 0.1f)
-        {
-            charController.Move(playerSpeed * Time.deltaTime * moveDir);
-            float rotationSpeed = 10.0f;
-            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
-        }
-        gravityVelocity.y += gravityAcceleration * Time.deltaTime;
-        charController.Move(gravityVelocity * Time.deltaTime);
+        //Vector3 moveDir = new Vector3(inputVector.x, 0.0f, inputVector.y).normalized;
+
+        //if(moveDir.magnitude >= 0.1f)
+        //{
+        //    charController.Move(playerSpeed * Time.deltaTime * moveDir);
+        //    float rotationSpeed = 10.0f;
+        //    transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+        //}
+        //gravityVelocity.y += gravityAcceleration * Time.deltaTime;
+        //charController.Move(gravityVelocity * Time.deltaTime);
     }
 }
